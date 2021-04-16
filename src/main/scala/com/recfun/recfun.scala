231eb8ad479d4520e5beb2e1a6465b490511b84d
li-lauren <laurenlidev@gmail.com>
2021-04-16 15:12:41 -0600

    Initial commit

---
+++ src/main/scala/com/recfun/recfun.scala
@@ -0,0 +1,33 @@
+package com.recfun
+
+object recfun {
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def _balance()
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
